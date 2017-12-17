9b8879297c350736d7950297b84e00d305f39608
YashdeepSharma <yashdeep.sharma@paytm.com>
2017-12-18 02:22:22 +0530

    All methods implemented with test cases covered

--- src/main/java/com/gojek/services/ParkingLotService.java
+++ src/main/java/com/gojek/services/ParkingLotService.java
@@ -2,10 +2,15 @@ package com.gojek.services;
 
 import com.gojek.exceptions.TicketNotIssuedException;
 import com.gojek.exceptions.VehicleNotFoundException;
+import com.gojek.models.Color;
 import com.gojek.models.Ticket;
 import com.gojek.models.Vehicle;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ParkingLotService {
 
@@ -18,9 +23,10 @@ public class ParkingLotService {
         this.ticketingSystem = new TicketingSystem(slots);
     }
 
-    public void parkVehicle(Vehicle vehicle) {
+    public int parkVehicle(Vehicle vehicle) {
         Ticket ticket = ticketingSystem.issueTicket();
         parkingLot.put(ticket.getSlot(), vehicle);
+        return ticket.getSlot();
     }
 
     public void removeVehicleFromSlot(int slot) {
@@ -30,5 +36,39 @@ public class ParkingLotService {
         } else {
             throw new VehicleNotFoundException();
         }
+    }
+
+    public List<String> findCarsLicensePlatesWithColor(Color color) {
+        return parkingLot
+                .entrySet()
+                .stream()
+                .filter(element -> element.getValue().getColor() == color)
+                .map(element -> element.getValue().getLicensePlate())
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> findSlotNumbersForColors(Color color) {
+        return parkingLot
+                .entrySet()
+                .stream()
+                .filter(element -> element.getValue().getColor() == color)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public Integer findSlotByRegistrationNumber(String registration) {
+        List<Integer> slot = parkingLot
+                .entrySet()
+                .stream()
+                .filter(element -> element.getValue().getLicensePlate().equalsIgnoreCase(registration))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        assert slot.size() == 1;
+        return slot.get(0);
+    }
+
+    public void getStatus() {
+        System.out.println("Slot No\tRegistration No.\tColour");
+        parkingLot.forEach((key, value) -> System.out.println(key + "\t\t" + value.getLicensePlate() + "\t\t" + value.getColor().getColor()));
     }
 }
--- src/test/java/com/gojek/services/ParkingLotServiceSpecs.java
+++ src/test/java/com/gojek/services/ParkingLotServiceSpecs.java
@@ -6,6 +6,10 @@ import com.gojek.models.Color;
 import com.gojek.models.Vehicle;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class ParkingLotServiceSpecs {
 
     @Test
@@ -17,6 +21,7 @@ public class ParkingLotServiceSpecs {
         parkingLotService.parkVehicle(new Vehicle("UP 13 AX 5386", Color.RED));
         parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9486", Color.WHITE));
         parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9388", Color.WHITE));
+        parkingLotService.getStatus();
     }
 
     @Test(expected = TicketNotIssuedException.class)
@@ -43,5 +48,45 @@ public class ParkingLotServiceSpecs {
         ParkingLotService parkingLotService = new ParkingLotService(1);
         parkingLotService.parkVehicle(new Vehicle("UP 14 AX 1386", Color.WHITE));
         parkingLotService.removeVehicleFromSlot(1);
+    }
+
+    @Test
+    public void findCarsWithColorsSpec() {
+        ParkingLotService parkingLotService = new ParkingLotService(6);
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 1386", Color.WHITE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9386", Color.BLACK));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9396", Color.BLUE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 5386", Color.RED));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9486", Color.WHITE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9388", Color.WHITE));
+        List<String> licensePlates = parkingLotService.findCarsLicensePlatesWithColor(Color.WHITE);
+        licensePlates.forEach(System.out::println);
+    }
+
+    @Test
+    public void findSlotByRegistrationNumberSpec() {
+        ParkingLotService parkingLotService = new ParkingLotService(6);
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 1386", Color.WHITE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9386", Color.BLACK));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9396", Color.BLUE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 5386", Color.RED));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9486", Color.WHITE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9388", Color.WHITE));
+        Integer slot = parkingLotService.findSlotByRegistrationNumber("UP 13 AX 9396");
+        assertTrue("Slot should be 3", slot == 3);
+    }
+
+    @Test
+    public void findSlotNumbersForColorsSpec() {
+        ParkingLotService parkingLotService = new ParkingLotService(6);
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 1386", Color.WHITE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9386", Color.BLACK));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9396", Color.BLUE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 5386", Color.RED));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9486", Color.WHITE));
+        parkingLotService.parkVehicle(new Vehicle("UP 13 AX 9388", Color.WHITE));
+        List<Integer> slots = parkingLotService.findSlotNumbersForColors(Color.WHITE);
+        assertTrue("slots should have size 3", slots.size() == 3);
+        System.out.println(slots);
     }
 }
